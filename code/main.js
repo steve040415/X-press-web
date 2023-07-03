74fa7e579fe9713f6f8b1e88e5f23fcd02261188
Mackenzie Tapp <m123tapp@gmail.com>
2023-07-03 14:24:59 -0230

    home almost done

--- code/main.js
+++ code/main.js
@@ -18,19 +18,16 @@ function buttonFunction() {
 const dropDown = document.querySelector('button');
 const navList = document.querySelectorAll("nav a");
 const nav = document.querySelector('nav');
-const tick = document.querySelector('.switch');
 dropDown.addEventListener("click", navMenu);
 function navMenu() {
     if(getComputedStyle(navList[0]).display=='none') {
         for(let i=0; i<navList.length; i++) {
             navList[i].style.display = "initial";
-            tick.style.display = "none";
         }
     }
     else {
         for(let i=0; i<navList.length; i++) {
             navList[i].style.display = "none";
-            tick.style.display = "inline-block";
         }
     }
 
