14fecd8637c9c841399d61ca0ae03095d8561803
steve040415 <m123tapp@gmail.com>
2023-08-15 20:27:57 -0230

    repo fixed

---
+++ Razor things/X-Press/Pages/Gallery.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace X_Press.Pages
+{
+    public class GalleryModel : PageModel
+    {
+        public static void Main(string[] args)
+        {
+        }
+        private readonly ILogger<GalleryModel> _logger;
+
+        public GalleryModel(ILogger<GalleryModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+
+}
+
---
+++ Razor things/X-Press/Pages/History.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace X_Press.Pages
+{
+    public class HistoryModel : PageModel
+    {
+        public static void Main(string[] args)
+        {
+        }
+        private readonly ILogger<HistoryModel> _logger;
+
+        public HistoryModel(ILogger<HistoryModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+
+}
