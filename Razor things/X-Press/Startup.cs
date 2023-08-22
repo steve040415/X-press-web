89fe3b5bd42081a8f3544c9078882786fb2c3cef
steve040415 <m123tapp@gmail.com>
2023-08-21 21:35:22 -0230

    attempt at razor components

---
+++ Razor things/X-Press/Startup.cs
@@ -0,0 +1,35 @@
+﻿public class Startup
+
+{
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorPages();
+
+        services.AddServerSideBlazor();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+
+            endpoints.MapBlazorHub();
+        });
+    }
+}
